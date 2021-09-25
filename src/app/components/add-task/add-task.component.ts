918c2da02f1ddf51629aca4121de93528771ef6e
GustavoSantos16 <37877573+GustavoSantos16@users.noreply.github.com>
2021-09-24 23:40:39 -0300

    toggle button subject observable

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from '../../Task';
 @Component({
   selector: 'app-add-task',
@@ -11,8 +13,12 @@ export class AddTaskComponent implements OnInit {
   text: string = "";
   day: string ="";
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription : Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-   title: string = "Task Tracker"
-  constructor() { }
+   title: string = "Task Tracker";
+   showAddTask: boolean = false;
+   subscription!: Subscription;
+ 
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
 
 
   toggleAddTask(){
-    alert('here');
+    this.uiService.toggleAddTask();
   }
 }
